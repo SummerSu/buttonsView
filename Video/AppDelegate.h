ed157402ea9bb23d9d730c2b2277cf6bb94a6a54
ss <1274005014@qq.com>
2016-12-21 15:18:48 +0800

    Initial Commit

---
+++ Video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Video
+//
+//  Created by ss on 2016/12/21.
+//  Copyright © 2016年 ss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
